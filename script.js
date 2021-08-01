d46b3a03085827e07705e36445c8ad23b54a61bb
woojae05 <dnwo05@naver.com>
2021-08-01 23:25:38 +0900

    creat:dataType

---
+++ script.js
@@ -0,0 +1,37 @@
+// //String
+// const what= "woojae";
+// console.log(what);
+
+// //Boolean
+// const wat = true;
+// console.log(wat);
+
+// //Number 
+// const wat2 = 666;
+
+// //float
+// const wat3=55.1;
+
+//array
+// const daysOfWeek = ["mon","tue","Wed","Thu","Fri","Sat","sun"];
+// console.log(daysOfWeek[2]);
+
+//object
+const wooInfo={
+    name:"woojae",
+    age:17,
+    gender:"Male",
+    isHandsome:true,
+    favmovies: ["zero","zzangu"],
+    favfood:[
+      {
+        name:"kimch",
+        fatty:false
+      },
+      {
+        name:"hamburger",
+        fatty:true
+      }
+    ]
+  };
+  console.log(wooInfo.favfood[1].name);
