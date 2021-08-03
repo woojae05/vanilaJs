0aa49b3ad7f85dfc3467d989e843409e8fc56d44
woojae05 <dnwo05@naver.com>
2021-08-03 21:00:30 +0900

    creat:if

--- script.js
+++ script.js
@@ -85,23 +85,85 @@
 
 // const title=
 // document.getElementById("title")
-const title=document.querySelector("#title");
+// const title=document.querySelector("#title");
 
-console.log(title);
+// console.log(title);
 
-// DOM
-title.innerHTML = "Hi! from Js";
-title.style.color ="yellow";
-document.title="hello";
+// // DOM
+// title.innerHTML = "Hi! from Js";
+// title.style.color ="yellow";
+// document.title="hello";
 
-//window
-function handleResize(event){
-    console.log(event);
-}
+// //window
+// function handleResize(event){
+    //     console.log(event);
+    // }
+    
+    // window.addEventListener("resize", handleResize);
+    
+// function handleClick(){
+//         if(title.style.color=="blue")
+//         title.style.color="black";
+//         else title.style.color="blue";17
+//     };
+//     title.addEventListener("click",handleClick);
+    
 
-window.addEventListener("resize", handleResize);
+    // // if
+    // let age = prompt("how old are you?");
+    // if(age>=18&&age<=21){1
+    //     console.log("you can drink");
+    // }else if(age>21){
+    //     console.log("go ahead");
+    // }else{
+    //     console.log("too young");
+    // }
 
+// const title = document.querySelector("#title");
+
+// const BASE_COLOR="rgb(52, 73, 94)";
+// const OTHER_COLOR="#7f8c8d";
+
+// function handleClick(){
+//    const currentColor = title.style.color;
+//    if(currentColor===BASE_COLOR){
+//        title.style.color=OTHER_COLOR;
+//    }
+//    else{
+//        title.style.color=BASE_COLOR;
+//    }
+// }
+
+// function init(){
+//     title.style.color=BASE_COLOR;
+//     title.addEventListener("click",handleClick);
+// }
+// init();
+// title.addEventListener("click",handleClick);
+
+// function handleOnline(){
+//     console.log("welcom back");
+// }
+// function handleOffline(){
+//     console.log("Bye bye");
+// }
+
+// window.addEventListener("online",handleOnline);
+// window.addEventListener("offline",handleOffline);
+
+const title = document.querySelector("#title");
+
+const CLICKED_CLASS="clicked";
 function handleClick(){
-    title.style.color="blue";
-};
-title.addEventListener("click",handleClick);
+    title.classList.toggle(CLICKED_CLASS);
+    // const hasClass = title.classList.contains(CLICKED_CLASS);
+    // if(hasClass){
+    //     title.classList.remove(CLICKED_CLASS);
+    // }else{
+    //     title.classList.add(CLICKED_CLASS);
+    // }
+}
+function init(){
+    title.addEventListener("click",handleClick);
+}
+init();
